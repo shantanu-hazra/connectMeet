533b2e5e3fc038ea5822c605e98a30b072e8c393
Shantanu <hazrashantanu051@gmail.com>
2025-06-09 13:33:52 +0530

    Uploading the project files

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,6 @@
-let IS_PROD = false;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
-    "http://localhost:8080"
-
+let IS_PROD = true;
+const server = IS_PROD
+  ? `${import.meta.env.EXPRESS_SERVER_ENDPOINT}`
+  : "http://localhost:8080";
 
 export default server;
