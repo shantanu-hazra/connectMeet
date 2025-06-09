c16ba3645207a972231bd4e4108dd620b48b2c20
Shantanu <hazrashantanu051@gmail.com>
2025-06-09 16:59:33 +0530

    Uploading the project files

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { login, register,AddToHistory,getUserHistory } from "../controllers/users.controllers.js";
-
-
+import {
+  login,
+  register,
+  AddToHistory,
+  getUserHistory,
+} from "../controllers/users.controllers.js";
 
 const router = Router();
 
 router.route("/login").post(login);
 router.route("/register").post(register);
-router.route("/add_to activity").post(AddToHistory);
+router.route("/add_to_activity").post(AddToHistory);
 router.route("/get_all_activity").get(getUserHistory);
 
 export default router;
