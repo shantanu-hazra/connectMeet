7ec3cab2bc034e7ce464806026eaf674e2ea1dab
Shantanu <hazrashantanu051@gmail.com>
2025-06-09 17:07:08 +0530

    Uploading the project files

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? `${import.meta.env.EXPRESS_SERVER_ENDPOINT}`
+  ? `${import.meta.env.VITE_EXPRESS_SERVER_ENDPOINT}`
   : "http://localhost:8080";
 
 export default server;
