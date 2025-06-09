7a34f19703227bbea4d0950ff9bec925e98ec432
Shantanu <hazrashantanu051@gmail.com>
2025-06-09 15:59:33 +0530

    Uploading the project files

--- frontend/src/utils/withAuth.jsx
+++ frontend/src/utils/withAuth.jsx
@@ -1,25 +1,22 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-const withAuth =(WrappedComponent) =>{
-    const AuthComponent = (props) =>{
-        const router = useNavigate();
+const withAuth = (WrappedComponent) => {
+  const AuthComponent = (props) => {
+    const router = useNavigate();
 
-        const isAuthenticated = () =>{
-            if(localStorage.getItem("token")){
-                return item;
-            }
-            return false
-        }
+    const isAuthenticated = () => {
+      return !!localStorage.getItem("token");
+    };
 
-        useEffect(()=>{
-            if(!isAuthenticated()){
-                router('/auth')
-            }
-        },[])
-    }
+    useEffect(() => {
+      if (!isAuthenticated()) {
+        router("/auth");
+      }
+    }, []);
+  };
 
-    return AuthComponent
-}
+  return AuthComponent;
+};
 
 export default withAuth;
